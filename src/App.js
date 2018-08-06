29439cdf7b67bbecad57b8f8629e812c245cfbbc
vanceleon <vanceleon44@gmail.com>
2018-08-06 17:41:27 -0400

    js and css files added in src

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummyData';
+// import SearchBar from 
+
 
 class App extends Component {
   render() {
@@ -11,7 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-
+      
       </div>
     );
   }
